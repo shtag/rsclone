87f8d4e2f9683789069d599634252918166f59c1
shtag <vasyakanishchev@gmail.com>
2023-02-01 17:07:52 +0200

    feat: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/app.ts'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s?css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                use: "html-loader",
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                type: 'asset/resource'
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/img/[name][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/_redirects' ,
+                    to: '',
+                    globOptions: {
+                        ignore: ['*.ts', '*.scss', '*.js'],
+                    }
+                }
+            ],
+        }),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
