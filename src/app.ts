9849ddd084cb5becabcff4f49326f5d3d2155830
Kiryl Astapenka <81929875+astap13@users.noreply.github.com>
2023-02-05 05:52:15 +0100

    feat: fix style static elements and add toggler of theme

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-// eslint-disable-next-line import/extensions
-import Header from './home-page/header/view';
+import {Header} from './pages/home-page/header/view';
+import './scss/styles.scss';
+import './pages/home-page/header/style.scss'
 
 class App {
     header: Header;
