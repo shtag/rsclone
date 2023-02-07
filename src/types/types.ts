e4d7440085e528365d91a80c1f2b5ed481894330
Maria Gornashevich <lightbrown9874@gmail.com>
2023-02-08 02:51:51 +0300

    feat: add api requests for users

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,24 @@ export interface PostData {
     image: string;
     description: string;
 }
+
+export interface UserInfo {
+    sessionId?: string;
+    username?: string;
+    password?: string;
+    id?: number;
+}
+
+export interface UserSettings {
+    sessionId: string;
+    settings: {
+        photo?: string;
+        language?: string;
+        name?: string;
+        descriptionProfile?: string;
+    };
+}
+
+export interface UserList {
+    list: string[];
+}
