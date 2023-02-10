c0b87f0c7ee57e53f213b0914e04c43d951d9b1c
Vasyl Kanishchev <vasyakanishchev@gmail.com>
2023-02-11 01:34:54 +0200

    feat: set local.storage

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,13 @@ import PageController from './pages/PageController';
 import './pages/home-page/staticElements/style.scss';
 import './pages/home-page/postElements/style.scss'
 import Router from './router';
+import { sessionId } from './types/constants';
 
 class App {
     static async start() {
         console.log('start');
+        localStorage.setItem('sessionId', sessionId);
+        localStorage.setItem('userId', '1');
         Router.setEventListeners();
         PageController.renderStructure();
         PageController.setControllers();
