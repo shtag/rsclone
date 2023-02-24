e395090c7c1fe9cd8657588dbb1f3eb442db448b
Maria Gornashevich <lightbrown9874@gmail.com>
2023-02-24 03:37:40 +0300

    feat: apply changes in settings layout

--- src/pages/settings/SettingsView.ts
+++ src/pages/settings/SettingsView.ts
@@ -20,31 +20,36 @@ class SettingsView {
               <div class="text_button">Save changes</div>
            </button>
         </form>
-        <form class="settings__form-interaction">
-           <div class="settings__interaction">
-              <h2 class="settings__subtitle">User interaction</h2>
-              <div class="settings__theme-block">
-                 <h3 class="settings__block-title">Theme: </h3>
-                 <input class="settings__theme" type="checkbox" name="themeAuto" id="themeAuto" value="themeAuto">
-                 <label for="themeAuto">Automatically change theme</label>
-              </div>
-              <div class="settings__lang-block">
-                 <h3 class="settings__block-title">Language:</h3>
-                 <input type="checkbox" class="settings__lang">
-              </div>
+        <div class="settings__interaction">
+           <h2 class="settings__subtitle">User interaction</h2>
+           <div class="settings__theme-block">
+              <h3 class="settings__block-title">Theme: </h3>
+              <input class="settings__theme" type="checkbox" name="themeAuto" id="themeAuto" value="themeAuto">
+              <label for="themeAuto">Automatically change theme</label>
+              <img class="settings__hint_icon" src="/img/icons/hint-icon.svg"/>
+              <div class="settings__hint">You don't need to click submit button to apply theme changes. It's done automatically.</div>
+           </div>
+           <div class="settings__lang-block">
+              <h3 class="settings__block-title">Language:</h3>
+              <input type="checkbox" class="settings__lang">
+           </div>
+           <form class="settings__form-interaction">
               <div class="settings__name-block">
                  <label class="settings__label">
-                 Name:<input type="name" name="name" class="settings__input settings__input_name" placeholder="Enter new name">
+                 Name:<input type="name" name="name" class="settings__input settings__input_name" maxlength="22" placeholder="Enter new name">
+                 <span class="settings__error_username"></span>
                  </label>
               </div>
               <div class="settings__decription-block">
                  <h3 class="settings__block-title">Profile description:</h3>
-                 <textarea class="settings__description" name="description" maxlength="150"></textarea>
+                 <textarea class="settings__description" name="description" maxlength="150" placeholder="Enter new description"></textarea>
+                 <span class="settings__error_description"></span>
               </div>
-           </div>
-           <button type="submit" class="settings__submitInteraction open__post-btn" disabled>
-              <div class="text_button">Save changes</div>
-           </button>
+        </div>
+        <button type="submit" class="settings__submitInteraction open__post-btn" disabled>
+        <div class="text_button">Save changes</div>
+        </button>
+        <span class="settings__error_submit"></span>
         </form>
         `;
     }
